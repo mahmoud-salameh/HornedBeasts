cf20715592d7ab3cdcc3dabfd05c3614e0305911
mahmoud-salameh <mahmoud4561@hotmail.com>
2021-06-07 02:37:23 +0300

    redoing for some changes

--- lab01/src/component/main.js
+++ lab01/src/component/main.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import HornedBeasts from './hornedbeast';
 
-class Main extends React.Component{
-    render(){
-        return(
-            <div>
-                <HornedBeasts imageUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'} description={'A unicorn and a narwhal nuzzling their horns'}/>
-                <HornedBeasts  imageUrl={'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'} description={'Mother (or father) rhino with two babies'}/>
-                <HornedBeasts  imageUrl={'https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'} description={'Someone wearing a creepy unicorn head mask'}/>
-            </div>
-            
+class Main extends React.Component {
+
+    render() {
+
+        const imageUrl1 = "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg";
+
+        const imageUrl2 = "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80";
+
+        const imageUrl3 = "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg";
+
+        return (
+            <main>
+                <div>
+                    <title>UniWhal</title>
+
+                    <img src={imageUrl1} alt="" />
+
+                    <p>A unicorn and a narwhal nuzzling their horns</p>
+
+
+                    <title>Rhino Family</title>
+
+                    <img src={imageUrl2} alt="" />
+
+                    <p>Mother (or father) rhino with two babies</p>
+
+
+
+
+                    <title>Unicorn Head</title>
+
+                    <img src={imageUrl3} alt="" />
+
+                    <p>Someone wearing a creepy unicorn head mask</p>
+
+                </div>
+            </main>
         )
     }
 }
+
 export default Main;
