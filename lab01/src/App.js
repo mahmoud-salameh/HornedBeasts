0c8046948f844423236ed7507e48a4e5aa36abad
mahmoud-salameh <mahmoud4561@hotmail.com>
2021-06-09 02:17:20 +0300

    the lab03 but need stil some work

--- lab01/src/App.js
+++ lab01/src/App.js
@@ -2,16 +2,52 @@ import React from 'react';
 import Header from './component/header';
 import Footer from './component/footer';
 import Main from './component/main';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SelectedBeast from './component/SelectedBeast';
+import imgData from './component/data.json';
+
+
+
+
 class App extends React.Component {
+
+constructor(props) {
+
+  super(props);
+  this.state = {
+
+    beastArray: imgData, show: false,
+
+  }
+}
+
+closeIfFalse = ()=> {
+  this.setState({show:false})
+}
+openIfTrue = ()=> {
+  this.setState({show:true})
+}
+
+
+
+
+
   render() {
     return (
       
       <div>
         <Header />
 
-        <Main />
+        <Main beastArray = {this.state.beastArray} openIfTrue={this.openIfTrue}/>
 
         <Footer />
+        <SelectedBeast show={this.state.show}
+                closeIfFalse={this.closeIfFalse}
+                  title={imgData.title}
+                  img={imgData.img}
+                   description={imgData.description} 
+                   />
+                   
       </div>
     )
   }
